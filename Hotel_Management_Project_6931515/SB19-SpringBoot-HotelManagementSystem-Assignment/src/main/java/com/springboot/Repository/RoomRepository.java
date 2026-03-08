bd16433454c5ff9b6717206a642f7c0cc551eee3
Kanav.Jethi-12215538 <117577182+KanavJETHI@users.noreply.github.com>
2026-03-08 09:36:53 +0530

    Delete Hotel_Management_Project_6931515/SB19-SpringBoot-HotelManagementSystem-Assignment directory

--- Hotel_Management_Project_6931515/SB19-SpringBoot-HotelManagementSystem-Assignment/src/main/java/com/springboot/Repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.Repository;
-import org.springframework.data.repository.CrudRepository;
-
-
-import com.springboot.Entity.Room;
-
-public interface RoomRepository extends CrudRepository<Room, String> {
-
-}
